23735af95ab9f7af8fb4fa4d058ee18bc15cb899
jona27081 <ZS20006736@ESTUDIANTES.UV.MX>
2023-09-19 03:05:38 -0600

    marge to main

---
+++ CRUD-PHP/src/crud.php
@@ -0,0 +1,125 @@
+<?php
+session_start();
+
+// Verificar si el usuario ha iniciado sesión
+if (!isset($_SESSION["usuario"])) {
+    header("Location: index.php"); // Redireccionar al usuario a la página de inicio de sesión si no ha iniciado sesión
+    exit();
+}
+
+if (isset($_POST["logout"])) {
+    session_destroy();
+    header("Location: index.php");
+    exit();
+}
+
+$host = "172.17.0.2";
+$port = "5432";
+$dbname = "alumnos";
+$user = "postgres"; 
+$password = "postgres"; 
+
+$edit_mode = false; // Variable para controlar el modo de edición
+
+try {
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error en la conexión a la base de datos: " . $e->getMessage());
+}
+
+// Verifica si se ha enviado el formulario de alta de alumnos
+if (isset($_POST["insert"])) {
+    // Obtener los valores del formulario
+    $nombre = $_POST["nombre"];
+    $direccion = $_POST["direccion"];
+    $telefono = $_POST["telefono"];
+    $correo = $_POST["correo"];
+
+    try {
+        // Consulta SQL para insertar un nuevo registro en la tabla "alumnos"
+        $sentencia = $pdo->prepare("INSERT INTO alumnos (nombre, direccion, telefono, correo) VALUES (?, ?, ?, ?)");
+        $resultado = $sentencia->execute([$nombre, $direccion, $telefono, $correo]);
+
+        if ($resultado === true) {
+            // Redirigir a la página principal después de insertar
+            header("Location: crud.php");
+            exit(); // Importante: terminar el script después de redirigir
+        } else {
+            echo "Error al insertar el alumno en la base de datos.";
+        }
+    } catch (PDOException $e) {
+        echo "Error en la consulta a la base de datos: " . $e->getMessage();
+    }
+}
+
+// Código para obtener la lista de alumnos
+try {
+    // Consulta SQL para seleccionar datos de la tabla "alumnos"
+    $sentencia = $pdo->query("SELECT id, nombre, direccion, correo, telefono FROM alumnos");
+
+    // Obtener los datos en forma de objetos
+    $alumnos = $sentencia->fetchAll(PDO::FETCH_OBJ);
+} catch (PDOException $e) {
+    echo "Error en la consulta a la base de datos: " . $e->getMessage();
+}
+
+// Código para eliminar un alumno
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+
+    try {
+
+        $sentencia = $pdo->prepare("DELETE FROM alumnos WHERE id = ?;");
+        $resultado = $sentencia->execute([$id]);
+
+        if ($resultado === true) {
+            header("Location: crud.php");
+            exit();
+        } else {
+            echo "Algo salió mal";
+        }
+    } catch (PDOException $e) {
+        echo "Error en la conexión o consulta a la base de datos: " . $e->getMessage();
+    }
+}
+
+// Código para cargar los datos del alumno seleccionado para edición
+if (isset($_GET["edit_id"])) {
+    $edit_id = $_GET["edit_id"];
+    $edit_mode = true; // Establece el modo de edición
+    
+    try {
+
+        $sentencia = $pdo->prepare("SELECT * FROM alumnos WHERE id = ?;");
+        $sentencia->execute([$edit_id]);
+        $alumno_edit = $sentencia->fetch(PDO::FETCH_OBJ);
+    } catch (PDOException $e) {
+        echo "Error en la conexión o consulta a la base de datos: " . $e->getMessage();
+    }
+}
+
+// Código para actualizar los datos del alumno
+if (isset($_POST["update"])) {
+    $id = $_POST["id"];
+    $nombre = $_POST["nombre"];
+    $direccion = $_POST["direccion"];
+    $telefono = $_POST["telefono"];
+    $correo = $_POST["correo"];
+
+    try {
+
+        $sentencia = $pdo->prepare("UPDATE alumnos SET nombre = ?, direccion = ?, correo = ?, telefono = ? WHERE id = ?;");
+        $resultado = $sentencia->execute([$nombre, $direccion, $correo, $telefono, $id]);
+
+        if ($resultado === true) {
+            header("Location: crud.php");
+            exit();
+        } else {
+            echo "Algo salió mal al actualizar el alumno";
+        }
+    } catch (PDOException $e) {
+        echo "Error en la conexión o consulta a la base de datos: " . $e->getMessage();
+    }
+}
+?>
